7fa39139652e7219dac20aaee945642d649d5824
Akshay Nair <phenax5@gmail.com>
2021-04-10 22:00:43 +0530

    Adds signature for Make

--- src/FreeEffects.ml
+++ src/FreeEffects.ml
@@ -5,7 +5,34 @@ module type EFFECT = sig
   val map : ('a -> 'b) -> 'a t -> 'b t
 end
 
-module Make(Eff: EFFECT) = struct
+module Make: functor(Eff: EFFECT) -> sig
+  type 'a t
+
+  val pure : 'a -> 'a t
+  val return : 'a -> 'a t
+
+  (* Lift an operation into an effect *)
+  val liftF : 'a Eff.t -> 'a t
+
+  (* The opposite of liftF *)
+  val retract : 'a t -> 'a t Eff.t option
+
+  (* map over an effect to change the operation inside *)
+  val hoist : ('a t Eff.t -> 'a t Eff.t) -> 'a t -> 'a t
+
+  (* Run two effects in sequence *)
+  val flatMap : ('a -> 'b t) -> 'a t -> 'b t
+
+  (* Run two effects in sequence ignoring the result of the first one *)
+  val seq : 'a t -> 'b t -> 'a t
+
+  (* Alias to seq *)
+  val then_ : 'a t -> 'b t -> 'a t
+
+  (* Fold the sequence of effects into a result using a handler function *)
+  val fold : ('a t Eff.t -> 'a t) -> 'a t -> 'a
+
+end = functor(Eff: EFFECT) -> struct
   type 'a t = 
     | Pure of 'a
     | Free of 'a t Eff.t
@@ -23,14 +50,6 @@ module Make(Eff: EFFECT) = struct
     | Pure x -> Pure x
     | Free m -> Free (fn m)
 
-  (* map : ('a -> 'b) -> 'a t -> 'b t *)
-  (* join : ('a t) t -> 'a t *)
-
-  (* Async helpers (promise) *)
-  (* Use bastet *)
-  (* Use GADTs *)
-  (* Infix operators *)
-
   let rec flatMap fn = function
     | Pure a -> fn a
     | Free m -> Free (Eff.map (flatMap fn) m)
@@ -40,7 +59,7 @@ module Make(Eff: EFFECT) = struct
 
   let rec fold fn = function
     | Pure x -> x
-    | Free m -> fold fn (fn m)
+    | Free m -> fn m |> fold fn
 end
 
 (*module Compose(A: EFFECT)(B: EFFECT) = struct*)
